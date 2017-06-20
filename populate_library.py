b246c8544e5c8e9430108c0e30a84c7ac2cfbb06
evenflow22 <vincent.libucki@gmail.com>
2017-06-20 19:31:07 -0400

    midterm

--- populate_library.py
+++ populate_library.py
@@ -1,47 +1,57 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', midterm.settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'midterm.settings')
 
-import.django
+import django
 django.setup()
 from library.models import Book, DVD, Magazine, Rental, User
 
 
 
 def add_book(book_title, author, ISBN, genre, image):
-    b=Book.objects.getorcreate
-    b.book_title = book_title
+    b = Book.objects.get_or_create(book_title=book_title)[0]
     b.author = author
     b.ISBN = ISBN
     b.genre = genre
     b.image =image
+    b.save()
 
 def add_dvd(dvd_title, director, genre, image):
-    d=DVD.objects.getorcreate
-    d.dvd_title = dvd_title
+    d = DVD.objects.get_or_create(dvd_title=dvd_title)[0]
     d.director = director
     d.genre = genre
     d.image = image
+    d.save()
 
 def add_magazine(magazine_title, magazine_publisher, image):
-    m=Magazine.objects.getorcreate
-    m.magazine_title=magazine_title
+    m = Magazine.objects.get_or_create(magazine_title=magazine_title)[0]
     m.magazine_publisher=magazine_publisher
     m.image=image
+    m.save()
 
 def add_user(customer_firstname, customer_lastname, customer_balance):
-    u=User.objects.getorcreate
-    u.customer_firstname=customer_firstname
+    u = User.objects.get_or_create(
+            customer_firstname=customer_firstname)[0]
     u.customer_lastname=customer_lastname
     u.customer_blanace=customer_balance
+    u.save()
 
-def add_rental(borrower, borrower_item, status, date_due):
-    r=Rental.objects.getorcreate
-    r.borrower=borrower
-    r.borrower_item=borrower_item
+def add_rental(borrower, borrowed_item, status, date_due)
+    r = Rental.objects.get_or_create(
+              borrower=borrower)[0]
+    r.borrowed_item=borrowed_item
     r.status=status
     r.date_due=date_due
+
+
+
+
+def populate():
+    add_book("Of Mice and Men", "House of Nails",)
+    add_dvd("Space Balls", "Office Space", "Top Gun", None)
+    add_magazine("Person of the Year: Facebook's Mark Zuckerberg", None)
+    add_user("Vincent", "Libucki", 20)
+    add_borrower("Vincent", "Libucki")
 
 if __name__ == '__main__':
 
     populate()
-
